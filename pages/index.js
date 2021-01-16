633314dfb35890640a61cd6b9f1a8b6a8ad02547
zzgauravsharma <zzgauravsharma@royalenfield.com>
2021-01-17 01:59:06 +0530

    Added Meta tags

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,36 @@ import Homepage from '../components/Homepage';
 
 function Home(props) {
   return (
-    <div className='container-fluid'>
+    <div className="container-fluid">
       <Head>
         <title>SpaceX</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1,user-scalable=no,viewport-fit=cover, minimum-scale=1, maximum-scale=1"
+        />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="SpaceX - Launches" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="og:title" property="og:title" content="SpaceX - Launches" />
+        <meta property="og:type" content="website" />
+        <meta name="keywords" content="SpaceX - Launches" />
+        <meta
+          property="og:image"
+          content="https://www.spacex.com/static/images/share.jpg"
+        />
+        <meta property="og:description" content="SpaceX Launches Description" />
         <link
-          rel='icon'
-          href='https://www.spacex.com/static/images/favicon.ico'
+          rel="icon"
+          href="https://www.spacex.com/static/images/favicon.ico"
         />
       </Head>
 
       <main>
         <h4 className={''}>SpaceX Launch Programs</h4>
       </main>
-      <Homepage {...props}/>
+      <Homepage {...props} />
       <footer>
         <b>Developed By:</b> Gaurav Sharma
       </footer>
@@ -27,7 +44,7 @@ function Home(props) {
 
 export async function getServerSideProps(context) {
   return {
-    props: {params: context.query}
+    props: { params: context.query },
   };
 }
 
